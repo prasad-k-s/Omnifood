c1bf8ac823c182f71fac748e43538927af1545fb
Prasad K S <prasad07062001@gmail.com>
2024-04-16 15:34:59 +0530

    fix bugs and update code

--- js/script.js
+++ js/script.js
@@ -28,11 +28,13 @@ allLinks.forEach(function (link) {
         behavior: "smooth",
       });
     }
+    //Scroll to other links
     if (href !== "#" && href.startsWith("#")) {
       const sectionEl = document.querySelector(href);
       sectionEl.scrollIntoView({ behavior: "smooth" });
     }
 
+    //Close mobile navigation
     if (link.classList.contains("main-nav-link")) {
       headerEl.classList.toggle("nav-open");
     }
